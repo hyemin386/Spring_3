81b8f5c1d585a2e0224640e9cdcf0a8a0b3209f3
hyemin386 <ha_ha24@naver.com>
2021-03-22 17:44:36 +0900

    bankbook step1

---
+++ src/main/java/com/iu/s3/bankBook/BankBookService.java
@@ -0,0 +1,25 @@
+package com.iu.s3.bankBook;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BankBookService {
+	
+	@Autowired
+	private BankBookDAO bankBookDAO;
+	
+	public BankBookDTO getSelect(BankBookDTO bankBookDTO) throws Exception {
+		return bankBookDAO.getSelect(bankBookDTO);
+	}
+	
+	public List<BankBookDTO> getList() throws Exception {
+		return bankBookDAO.getList();
+	}
+	
+	public int setWrite(BankBookDTO bankBookDTO) throws Exception {
+		return bankBookDAO.setWrite(bankBookDTO);
+	}
+}
