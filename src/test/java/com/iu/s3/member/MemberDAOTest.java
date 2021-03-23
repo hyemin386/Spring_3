5581e25b33c9b6711a6adb547e66256ef82b00d4
hyemin386 <ha_ha24@naver.com>
2021-03-23 14:20:37 +0900

    member step1

---
+++ src/test/java/com/iu/s3/member/MemberDAOTest.java
@@ -0,0 +1,13 @@
+package com.iu.s3.member;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MemberDAOTest {
+
+	@Autowired
+	private MemberDAO memberDAO;
+
+}
