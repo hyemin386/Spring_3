dbe83e4ea9d5d8b2deb1b7ee154460c9af99b8ec
hyemin386 <ha_ha24@naver.com>
2021-03-26 18:12:58 +0900

    notice getList

---
+++ src/test/java/com/iu/s3/notice/NoticeDAOTest.java
@@ -0,0 +1,23 @@
+package com.iu.s3.notice;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.iu.s3.MyAbstractTest;
+
+public class NoticeDAOTest extends MyAbstractTest {
+
+
+	@Autowired
+	private NoticeDAO noticeDAO;
+	
+	@Test
+	public void getListTets() throws Exception {
+		List<NoticeDTO> ar = noticeDAO.getList();
+		assertNotEquals(0, ar.size());
+	}
+}
