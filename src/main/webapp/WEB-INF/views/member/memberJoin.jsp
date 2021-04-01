4660a3884f4a028e7aba1958552ed957ace0c80e
hyemin386 <ha_ha24@naver.com>
2021-04-01 14:43:33 +0900

    memberJoin(javaScript)

--- src/main/webapp/WEB-INF/views/member/memberJoin.jsp
+++ src/main/webapp/WEB-INF/views/member/memberJoin.jsp
@@ -7,21 +7,30 @@
 <c:import url="../template/bootStrap.jsp"></c:import>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<style type="text/css">
+	.r1{
+		color: red;  /* 6글자 이상이면 빨간색 */
+	}
+	
+	.r2 {
+		color:blue;  /* 6글자 미만이면 파랑색 */
+	}
+</style>
 </head>
 <body>
 <c:import url="../template/header.jsp"></c:import>
-	
-	
 	<div class="container">
 	<h2>Member Join Page</h2>
 		<form action="./memberJoin" method="post">
 			<div class="form-group">
 				<label for="id">ID</label> 
 				<input type="text" class="form-control" id="id" name="id" aria-describedby="idHelp"> 
+				<h4 id="idResult"></h4>
 			</div>
 			<div class="form-group">
 				<label for="pw">Password</label> 
 				<input type="password" class="form-control" id="pw" name="pw">
+				<h4 id="pwResult"></h4>
 			</div>
 			<div class="form-group">
 				<label for="pw">Password</label> 
@@ -43,6 +52,6 @@
 			<input type="button" value="JOIN" class="btn btn-primary" onclick="memberJoin()">
 		</form>
 	</div>
-<script type="text/javascript" src="../resources/js/memberJoin.js"></script>
+<script type="text/javascript" src="../resources/js/memberJoin.js"></script>	
 </body>
 </html>
--- src/main/webapp/resources/js/memberJoin.js
+++ src/main/webapp/resources/js/memberJoin.js
@@ -1,29 +1,39 @@
 /**
  * 
  */
-function memberJoin() {
-	let id = document.getElementById("id");
-	if(id.value==""){
-		alert("필수입력입니다.");
+ //pw길이
+let id = document.getElementById("id");
+id.addEventListener("blur", function() {
+	let message = "6글자 미만입니다.";
+	let c = "r1";
+	if(id.value.length>5){
+		message = "6글자 이상입니다.";
+		c ="r2";
 	}
+	
+	let idResult = document.getElementById("idResult");
+	idResult.innerHTML = message;
+	idResult.setAttribute("class", c);
+});
 
-	let pw = document.getElementById("pw");
-	if(pw.value=""){
-		alert("필수입력입니다.");
+//pw길이 
+let pw = document.getElementById("pw");
+let pw2 = document.getElementById("pw2");
+pw.addEventListener("blur", function(){
+	let message ="8글자 미만입니다.";
+	if(pw.value.length>7){
+		message="8글자 이상입니다";
+		c ="r2";
 	}
 	
-	let name = document.getElementById("name");
-	if(name.value=""){
-		alert("필수입력입니다.");
+	let pwResult = document.getElementById("pwResult");
+	pwResult.innerHTML = message;
+	pwResult.setAttribute("class", c);
+});
+
+//pw와 pw2가 같은지 비교
+pw2.addEventListener("blur", function() {
+	if(pw.value != pw2.value) {
+		pw2.value="";
 	}
-	
-	let phone = document.getElementById("phone");
-	if(phone.value=""){
-		alert("필수입력입니다.");
-	}
-	
-	let email = document.getElementById("email");
-	if(email.value=""){
-		alert("필수입력입니다.");
-	}
-}	
+});
